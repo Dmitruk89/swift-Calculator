6896c2c9b9d185b4577bd5f41552370df9fc14c2
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2023-11-18 11:27:28 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mac on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
